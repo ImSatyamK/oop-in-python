73f9667acafb11964abce941846584e1d1f32ab9
ImSatyamK <189542961+ImSatyamK@users.noreply.github.com>
2025-12-02 12:43:46 +0530

    Initial commit

---
+++ 03_inheritence.py
@@ -0,0 +1,25 @@
+class Engineer:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+
+    def full_name(self):
+        return f"{self.fname} {self.lname}"
+    
+class SoftwareEngineer(Engineer):
+    def __init__(self, name, level, salary):
+        fname, lname = name.split(" ")
+        super().__init__(fname, lname)  # Calling the constructor of the parent class
+        self.level = level
+        self.salary = salary
+
+    def welcome(self):
+        return f"We welcome Mr. {self.full_name()} for the post of {self.level}."
+    
+    def salary_info(self):
+        return f"{self.full_name()}'s salary is {self.salary} USD."
+    
+se1 = SoftwareEngineer("John Doe", "Senior Dev", 90000)
+print(se1.full_name())        # Inherited method
+print(se1.welcome())     # Child class method
+print(se1.salary_info()) # Child class method
